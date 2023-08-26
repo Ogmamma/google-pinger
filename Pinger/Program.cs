865ffe21a5613a53a429e793a2f2127bcec5cb74
Ogmamma <tasicdimitrije@rocketmail.com>
2023-08-26 14:28:35 +0200

    added a constructor and did some oop

--- Pinger/Program.cs
+++ Pinger/Program.cs
@@ -1,23 +1,9 @@
-﻿using System.Net.NetworkInformation;
+﻿using Pinger;
+using System.Net.NetworkInformation;
 using System.Text;
 
 //Pinging Google DNS Server 
 
-Ping pingSender = new Ping();
-PingOptions options = new PingOptions();
-
-options.DontFragment = true;
-
-string data = "Ping";
-byte[] buffer = Encoding.ASCII.GetBytes(data);
-int timeout = 120;
-string address = "4.2.2.2"; 
-PingReply reply = pingSender.Send(address, timeout, buffer, options);
-
-if (reply.Status == IPStatus.Success)
-{
-    Console.WriteLine("Response: {0}", reply.Status.ToString());
-    Console.WriteLine("RoundTrip: {0}", reply.RoundtripTime);
-    Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
-    Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
-}
+PingService pingService = new PingService();
+var response = pingService.SendPing();
+Console.WriteLine(response);
